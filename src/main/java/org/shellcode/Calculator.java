b9a8f3dbc3c32d0268dc9da550c5b53875a7eb9c
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-04-04 18:57:35 +0200

    Added calculator implementation to make tests pass

--- src/main/java/org/shellcode/Calculator.java
+++ src/main/java/org/shellcode/Calculator.java
@@ -1,4 +1,17 @@
 package org.shellcode;
 
 public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+    public int divide(int a, int b) {
+        if (b!=0) return a/b;
+        else throw new IllegalArgumentException("Second parameter cannot be 0");
+    }
 }
