34177042a3ed0e196917fe8ecac6c0f0ed458dfe
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-04-24 02:32:57 +0200

    Added system prints to ask user necessary questions

--- src/main/java/org/treasuremap/Main.java
+++ src/main/java/org/treasuremap/Main.java
@@ -1,4 +1,39 @@
 package org.treasuremap;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int playerCount;
+    public static int mapSize;
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void validatePlayerCount() {
+
+    }
+
+    public static void validateMapSize() {
+
+    }
+
+    private static void gameQuestionPlayers() {
+        System.out.println("How many players?");
+        try {
+            playerCount = scan.nextInt();
+            validatePlayerCount();
+        } catch (Exception e) {
+            System.out.println("Invalid input, let's try that again!");
+            gameQuestionPlayers();
+        }
+    }
+
+    private static void gameQuestionMapSize() {
+        System.out.println("How big should the map be?");
+        try {
+            mapSize = scan.nextInt();
+            validateMapSize();
+        } catch (Exception e) {
+            System.out.println("Invalid input, let's try that again!");
+            gameQuestionMapSize();
+        }
+    }
 }
