02eb33a06570eb581d4acd67aa880e78741f5d62
PANINA19 <gcamilleri1997@gmail.com>
2019-04-24 14:20:47 +0200

    Added comemnts, minor changes to the code's logic

--- src/main/java/org/treasuremap/Player/Player.java
+++ src/main/java/org/treasuremap/Player/Player.java
@@ -3,10 +3,25 @@ import org.treasuremap.Position;
 
 public class Player {
 
+    /**
+     * The global variables declared here are:
+     * mapSize:         Useful fot the tileVisited 2-d array, and checking if a player is within bound of the map
+     * Position:        Each player will be placed in a specific x and y coordinate on the map
+     * tilesVisited:    A boolean 2-d array which shows which tiles have been uncovered,
+     *                  useful when generating the HTML files
+     */
+
+    private int mapSize;
     private Position position;
     private boolean[][] tilesVisited;
-    private int mapSize;
 
+    /**
+     * A contructor to create an object of type Player
+     * @param xCoord    - indicates the x-coordinate, used to set up the player's position
+     * @param yCoord    - indicates the y-coordinate, used to set up the player's position
+     * @param mapSize   - indicates the length of a side of the square map (in tiles),
+     *                    used to create the size of the 2d array of tilesVisited
+     */
     public Player (int xCoord, int yCoord, int mapSize){
         tilesVisited =  new boolean[mapSize][mapSize];
 
@@ -14,17 +29,32 @@ public class Player {
         this.mapSize = mapSize;
     }
 
+    /**
+     * Used to get the player's position
+     * @return position object
+     */
     public Position getPosition(){
         return position;
     }
 
-    public void setPosition(int x,int y){
+    /**
+     * Sets the player's position depending on the parameters below. Also updates the tilesVisited array to include the
+     * new tile uncovered.
+     * @param x - represents the x coordinate
+     * @param y - represents the y coordinate
+     */
+    public void setPosition(int x, int y){
         position.setX(x);
         position.setY(y);
         tilesVisited[x][y] = true;
     }
+
+    /**
+     * Tries to move the player 1 tile to the specified direction. If it goes out of the map boundaries or is an
+     * invalid character then the player doesn't move. When the move is valid, the player's position is updated.
+     * @param direction - a character indicating which tile the player should move to.
+     */
     public void move(char direction){
-        Position Position = position;
         int newX = position.getX();
         int newY = position.getY();
         switch (direction){
--- src/test/java/Player/PlayerTest.java
+++ src/test/java/Player/PlayerTest.java
@@ -1,57 +1,87 @@
-
-
 package Player;
+
+import org.treasuremap.Player.Player;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.treasuremap.Player.Player;
-import org.treasuremap.Position;
 
 public class PlayerTest{
     private Player player;
 
+
+    /**
+     * Sets up a new object of type Player, with the size of map as 10 and the player's position at (5,5)
+     */
     @Before
     public void setup() {
         player = new Player(5,5,10);
     }
 
+    /**
+     * Nullifies the player object
+     */
     @After
     public void cleanup() {
         player = null;
     }
 
+    /**
+     * Tests the move up function expects from the player's position of (5,5) to be updated to become (5,6)
+     */
     @Test
     public void playerMovesUpTest(){
         player.move('U');
         Assert.assertEquals(5,player.getPosition().getX());
         Assert.assertEquals(6,player.getPosition().getY());
     }
+
+    /**
+     * Tests the move down function expects from the player's position of (5,5) to be updated to become (5,4)
+     */
     @Test
     public void playerMovesDownTest(){
         player.move('D');
         Assert.assertEquals(5,player.getPosition().getX());
         Assert.assertEquals(4,player.getPosition().getY());
     }
+
+    /**
+     * Tests the move left function expects from the player's position of (5,5) to be updated to become (4,5)
+     */
     @Test
     public void playerMovesLeftTest(){
         player.move('L');
         Assert.assertEquals(4,player.getPosition().getX());
         Assert.assertEquals(5,player.getPosition().getY());
     }
+
+    /**
+     * Tests the move right function expects from the player's position of (5,5) to be updated to become (6,5)
+     */
     @Test
     public void playerMovesRightTest(){
         player.move('R');
         Assert.assertEquals(6,player.getPosition().getX());
         Assert.assertEquals(5,player.getPosition().getY());
     }
+
+    /**
+     * Tests the non-existant move "I" function thus expects from the player's position of (5,5) to be updated to
+     * become (5,5)
+     */
     @Test
     public void playerNonSpecifiedMoveTest(){
         player.move('I');
         Assert.assertEquals(5,player.getPosition().getX());
         Assert.assertEquals(5,player.getPosition().getY());
     }
+
+    /**
+     *  Sets the player's position to (0,0) and moves left. This causes teh request of moving outside of the map. Thus
+     *  we expect the player's position to go from (0,0) to (0,0).
+     */
     @Test
     public void playerInvalidMoveTest(){
         player.setPosition(0,0);
