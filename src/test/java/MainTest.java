af697cac5aa36ade0c41b1fa0ec72892a535a23f
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-04-24 02:50:02 +0200

    Adding failing tests for validation methods

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,60 @@
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.fail;
+import org.treasuremap.Main;
 
 public class MainTest{
+
+    @Test
+    public static void playerCountValidateAcceptTest(){
+        Main.playerCount = 5;
+        Assert.assertTrue(Main.validatePlayerCount());
+    }
+
+    @Test
+    public static void playerCountValidateRejectLowTest(){
+        Main.playerCount = 1;
+        Assert.assertFalse(Main.validatePlayerCount());
+    }
+
+    @Test
+    public static void playerCountValidateRejectHighTest(){
+        Main.playerCount = 1;
+        Assert.assertFalse(Main.validatePlayerCount());
+    }
+
+    @Test
+    public static void mapSizeSmallValidateAcceptTest(){
+        Main.playerCount = 3;
+        Main.mapSize = 5;
+        Assert.assertTrue(Main.validateMapSize());
+    }
+
+    @Test
+    public static void mapSizeLargeValidateAcceptTest(){
+        Main.playerCount = 5;
+        Main.mapSize = 8;
+        Assert.assertTrue(Main.validateMapSize());
+    }
+
+    @Test
+    public static void mapSizeSmallValidateRejectLowTest(){
+        Main.playerCount = 3;
+        Main.mapSize = 4;
+        Assert.assertFalse(Main.validateMapSize());
+    }
+
+    @Test
+    public static void mapSizeLargeValidateRejectLowTest(){
+        Main.playerCount = 5;
+        Main.mapSize = 7;
+        Assert.assertFalse(Main.validateMapSize());
+    }
+
+    @Test
+    public static void mapSizeValidateRejectHighTest(){
+        Main.playerCount = 5;
+        Main.mapSize = 51;
+        Assert.assertFalse(Main.validateMapSize());
+    }
+
 }
